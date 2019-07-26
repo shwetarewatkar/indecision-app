951521baea02f9ce9a626591e6ecc0d9508d7935
shwetarewatkar <shweta.rewatkar2@gmail.com>
2019-07-25 23:36:24 -0700

    rm global modules

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -6,8 +6,28 @@ class Counter extends React.Component{
         this.handleReset = this.handleReset.bind(this);
 
         this.state = {
-            count : props.count
+            count : 0
         }
+    }
+    componentDidMount(){
+        console.log("componentDidMount");
+        try{
+            const count = parseInt(localStorage.getItem('count'),10);
+            if(!isNaN(count)){
+                this.setState(() => ({ count}))
+            }
+            
+        }catch(e){
+            //do nothing
+        }
+    }
+    componentDidUpdate(prevProps,prevState){
+        console.log("componetDidUpdate");
+        if(prevState.count !== this.state.count){
+            const count = this.state.count;
+            localStorage.setItem('count', count);
+        }
+       
     }
     handleAddOne(){
         // console.log("+1");
@@ -46,9 +66,9 @@ class Counter extends React.Component{
     }
 }
 
-Counter.defaultProps = {
-    count: 0
-}
+// Counter.defaultProps = {
+//     count: 0
+// }
 ReactDOM.render(<Counter />,document.getElementById('app'));
 
 
