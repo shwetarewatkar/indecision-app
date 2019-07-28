1b46d4a0907f4ff234046e239b595499ad7b4f55
shwetarewatkar <shweta.rewatkar2@gmail.com>
2019-07-28 00:16:41 -0700

    scss added

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,11 +7,13 @@ const OptionModal = (props) =>{
         isOpen={!!props.selectedOption}
         contentLable="Selected Option"
         ariaHideApp={false}
+        closeTimeoutMS={110}
         onRequestClose={props.handleClearSelectedOption}
+        className="modal"
         >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleClearSelectedOption}>Ok</button>
+            <h3 className="modal__title">Selected Option</h3>
+            {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+            <button className="button" onClick={props.handleClearSelectedOption}>Ok</button>
         </Modal>
     )
 
